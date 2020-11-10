ed17666336c87efe68913d8d21c929dd9e1cd935
Barnabas Forgo <speedo@devo.hu>
2020-11-10 18:45:19 +0000

    test hooks-use-state

---
+++ packages/examples/hooks-use-state/src/example.test.tsx
@@ -0,0 +1,83 @@
+import { fireEvent, render, RenderResult } from '@testing-library/react'
+import React from 'react'
+import { Counter } from './example'
+
+describe('Counter', () => {
+  let component: RenderResult
+  let decrement: HTMLElement
+  let increment: HTMLElement
+
+  beforeEach(async () => {
+    component = render(<Counter />)
+    decrement = await component.findByText('-')
+    increment = await component.findByText('+')
+  })
+
+  it('should snapshot', () => {
+    expect(component.container).toMatchSnapshot()
+  })
+
+  it('should default to 0', () => {
+    expect(component.container).toHaveTextContent('0')
+  })
+
+  describe('when incrementing', () => {
+    beforeEach(() => {
+      fireEvent.click(increment)
+    })
+
+    it('should show 1', () => {
+      expect(component.container).toHaveTextContent('1')
+    })
+
+    describe('when incrementing', () => {
+      beforeEach(() => {
+        fireEvent.click(increment)
+      })
+
+      it('should show 2', () => {
+        expect(component.container).toHaveTextContent('2')
+      })
+    })
+
+    describe('when decrementing', () => {
+      beforeEach(() => {
+        fireEvent.click(decrement)
+      })
+
+      it('should show 0', () => {
+        expect(component.container).toHaveTextContent('0')
+      })
+    })
+  })
+
+  describe('when decrementing', () => {
+    beforeEach(() => {
+      fireEvent.click(decrement)
+    })
+
+    it('should show -1', () => {
+      expect(component.container).toHaveTextContent('-1')
+    })
+
+    describe('when decrementing', () => {
+      beforeEach(() => {
+        fireEvent.click(decrement)
+      })
+
+      it('should show -2', () => {
+        expect(component.container).toHaveTextContent('-2')
+      })
+    })
+
+    describe('when incrementing', () => {
+      beforeEach(() => {
+        fireEvent.click(increment)
+      })
+
+      it('should show 0', () => {
+        expect(component.container).toHaveTextContent('0')
+      })
+    })
+  })
+})
